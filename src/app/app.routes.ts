b5f3d8e049ba7fb1171f6ce224006ababeac8f44
Diego <dalonsorodriguez223@gmail.com>
2026-02-09 12:51:59 +0100

    Estructura inicial del proyecto Angular formada y  componentes generados.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { RegistroComponent } from './paginas/registro/registro.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { CarteleraComponent } from './paginas/cartelera/cartelera.component';
+import { PeliculaComponent } from './paginas/pelicula/pelicula.component';
+import { AsientosComponent } from './paginas/asientos/asientos.component';
+import { PerfilComponent } from './paginas/perfil/perfil.component';
+
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cartelera', component: CarteleraComponent },
+  { path: 'pelicula/:id', component: PeliculaComponent },
+  { path: 'asientos/:horarioId', component: AsientosComponent },
+  {path: 'perfil', component: PerfilComponent },
+
+  // Ruta comodín
+  { path: '**', redirectTo: '' }
+ ];
---
+++ src/app/paginas/asientos/asientos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asientos',
+  imports: [],
+  templateUrl: './asientos.component.html',
+  styleUrl: './asientos.component.css'
+})
+export class AsientosComponent {
+
+}
