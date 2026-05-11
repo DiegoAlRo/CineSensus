1ae3fdccec09615de858750828967cfd4fd54ef3
Diego <dalonsorodriguez223@gmail.com>
2026-05-11 20:24:19 +0200

    Commit Nº75, Pulido visual de Panel de Administrador terminado.
    Cambios:
    
    - Admin-layout.component.css: Se ha mejorado visualmente y añadido estilos responsive para móviles y tablets.
    
    - Admin-navbar.component(html ycss): Se ha mejorado visualmente y añadido estilos responsive para móviles y tablets.
    
    - Admin-sidebar.component.(html, ts y css): Se han mejorado sus estilos(Y añadido responsives), además de que se convertirá en un select en pantallas 480px.
    
    - Styler.css: Le he añadido la mayoría de los estilos css del panel del Administrador, ya que era mucho código duplicado.
    
    Nota:
    
    Con este commit, el pulido del css está terminad, aunque puede recivir cambios leves hasta la finalización definitiva del desarrollo del proyecto. El siguiente paso será cubrir errores y validaciones que he detectado durante el avance del css, el cual será seguido de la fase de pruebas definitiva, donde una vez esté decidido respecto a que la app ha alcanzado el contenido competente, comprobaré que todos sus apartados funcionan correctamente y están cubiertos de posibles fisuras.

--- src/app/paginas/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/app/paginas/admin/admin-sidebar/admin-sidebar.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-sidebar',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './admin-sidebar.component.html',
-  styleUrls: ['./admin-sidebar.component.css']
+  styleUrls: ['./admin-sidebar.component.css'],
 })
 export class AdminSidebarComponent {
+  constructor(private router: Router) {}
 
+  cambiarRuta(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    const ruta = select.value;
+    this.router.navigate([ruta]);
+  }
 }
